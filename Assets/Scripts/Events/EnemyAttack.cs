f07ae1e8dc04cd296056b33d43cd89efcf71e359
roesch <roesch@asta.uni-luebeck.de>
2018-01-27 00:09:54 +0100

    add first event structure

---
+++ Assets/Scripts/Events/EnemyAttack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : GameEvent {
+
+
+	public EnemyAttack(GameObject airplane)
+	{
+		//TODO: Camerashake
+
+		int count = Random.Range(1, 3);
+		//List<Station> stations = airplane.stations;
+
+		while (count < 0)
+		{
+			//int rand = Random.Range(0, stations.Count);
+			//stations[rand].InflictDamage(10);
+			//stations.Remove(stations[rand]);
+			count--;
+		}
+	}
+}
