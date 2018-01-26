486b933fd56f8f162999406d19a506f16362d99f
Maik Macho <nuda1998@gmail.com>
2018-01-26 22:53:18 +0100

    Player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour {
+
+    public float MovementSpeed = 10f;
+
+    Collider collider;
+    Rigidbody rigidbody;
+
+	void Awake() {
+        collider = GetComponent<Collider>();
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	void Update() {
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        rigidbody.velocity = new Vector3(
+            x * MovementSpeed,
+            rigidbody.velocity.y,
+            y * MovementSpeed
+        );
+
+	}
+
+}
