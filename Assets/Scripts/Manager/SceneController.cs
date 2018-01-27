b381bf44d6522e36ee90c0aea2de674bd9c68382
roesch <roesch@asta.uni-luebeck.de>
2018-01-27 13:29:49 +0100

    rotation shit for artists

--- Assets/Scripts/Manager/SceneController.cs
+++ Assets/Scripts/Manager/SceneController.cs
@@ -20,6 +20,8 @@ public class SceneController : MonoBehaviour {
     [SerializeField]
     ControlLamp evadeRightLamp;
 
+    private float minEventTime = 10f;
+    private float maxEventTime = 30f;
 
     private float timer;
     private float timeEvent;
@@ -33,18 +35,25 @@ public class SceneController : MonoBehaviour {
     }
 
     void Start(){
-        timeEvent = Random.Range(5f, 20f);
-        EnemyApproach ea = new EnemyApproach(airplane.enemyLamp);
+        timeEvent = Random.Range(minEventTime, maxEventTime);
     }
 
     private void Update()
     {
         timer += Time.deltaTime;
 
+        float difficultyIncrease = 0;
+
+        // increase difficulty every 10 seconds
+        if(Mathf.FloorToInt(Time.timeSinceLevelLoad) % 10 == 0)
+        {
+            difficultyIncrease++;
+        }
+
         if (timer >= timeEvent)
         {
             timer = 0;
-            timeEvent = Random.Range(5f, 20f); //TODO: make this faster as the game progresses
+            timeEvent = Random.Range(Mathf.Min(minEventTime - difficultyIncrease/2 , maxEventTime - difficultyIncrease) , Mathf.Max(minEventTime - difficultyIncrease / 2, maxEventTime - difficultyIncrease));
             TriggerRandomEvent();
         }
     }
