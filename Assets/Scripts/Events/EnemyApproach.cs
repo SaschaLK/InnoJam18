25399c21e3fadffb6c78beefe728bbf76ef80d06
Maik Macho <nuda1998@gmail.com>
2018-01-27 17:09:13 +0100

    Add NetworkIdentity to minigames

--- Assets/Scripts/Events/EnemyApproach.cs
+++ Assets/Scripts/Events/EnemyApproach.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemyApproach : GameEvent {
 
 	public EnemyApproach (SirenLamp lamp) {
+        if (lamp == null) return;
         Debug.Log("enemy is approaching");
         lamp.StartAlarm();
 	}
