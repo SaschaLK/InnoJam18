8fc6373e6353424b89b7c90e35613fb0f5fedfa4
Maik Macho <nuda1998@gmail.com>
2018-01-28 12:38:31 +0100

    Fix cam issue

--- Assets/Scripts/CameraTurner.cs
+++ Assets/Scripts/CameraTurner.cs
@@ -5,24 +5,15 @@ public class CameraTurner : MonoBehaviour {
 
     public float speed = 1.0f;
     public float amount = 1.0f;
-    Transform myTransform;
 
-    Vector3 origPos;
-    Vector3 ourPos;
-
-    void Start() {
-        myTransform = transform;
-    }
+    Vector3 prev;
 
     void Update() {
-        if (myTransform.localPosition != ourPos) {
-            origPos = myTransform.localPosition;
-        }
-
         float x = (Mathf.PerlinNoise(Time.time * speed + 1024f, 0f) * 2.0f - 1.0f) * amount;
         float y = (Mathf.PerlinNoise(0f, Time.time * speed + 2048f) * 2.0f - 1.0f) * amount;
         float z = (Mathf.PerlinNoise(Time.time * speed, Time.time * speed - 2048f) * 2.0f - 1.0f) * amount;
 
-        myTransform.localPosition = ourPos = origPos + new Vector3(x, y, z);
+        transform.Translate(-prev);
+        transform.Translate(prev = new Vector3(x, y, z));
     }
 }
