77d106b96912df9c5b6c544d7ce92497217ea80f
Maik Macho <nuda1998@gmail.com>
2018-01-28 12:29:10 +0100

    Fog of war, camera zoom

--- Assets/Scripts/CameraTurner.cs
+++ Assets/Scripts/CameraTurner.cs
@@ -1,20 +1,28 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-    public class CameraTurner : MonoBehaviour {
+public class CameraTurner : MonoBehaviour {
 
-		public float speed = 1.0f;
-		public float amount =1.0f;
-		Transform myTransform;
+    public float speed = 1.0f;
+    public float amount = 1.0f;
+    Transform myTransform;
 
-		void Start () {
-			myTransform = transform;
-		}
-			
-		void Update () {
-			float x = (Mathf.PerlinNoise(Time.time * speed, 0f) * 2.0f -1.0f) * amount;
-			float y = (Mathf.PerlinNoise(0f, Time.time * speed) * 2.0f - 1.0f) * amount;
+    Vector3 origPos;
+    Vector3 ourPos;
 
-			myTransform.localPosition = new Vector3(x,y,0f);
-		}
-	}
+    void Start() {
+        myTransform = transform;
+    }
+
+    void Update() {
+        if (myTransform.localPosition != ourPos) {
+            origPos = myTransform.localPosition;
+        }
+
+        float x = (Mathf.PerlinNoise(Time.time * speed + 1024f, 0f) * 2.0f - 1.0f) * amount;
+        float y = (Mathf.PerlinNoise(0f, Time.time * speed + 2048f) * 2.0f - 1.0f) * amount;
+        float z = (Mathf.PerlinNoise(Time.time * speed, Time.time * speed - 2048f) * 2.0f - 1.0f) * amount;
+
+        myTransform.localPosition = ourPos = origPos + new Vector3(x, y, z);
+    }
+}
